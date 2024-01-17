5d731f100403d4ccff2d3bd41cf43c384bf3c2c1
amirholakoo <amirholakoo@gmail.com>
2024-01-17 06:11:14 -0800

    Update get_materials.php

--- get_materials.php
+++ get_materials.php
@@ -4,15 +4,15 @@ include 'connect_db.php';
 if (isset($_GET['supplier_id'])) {
     $supplierID = $_GET['supplier_id'];
 
-    $query = "SELECT MaterialID, MaterialName FROM RawMaterials WHERE SupplierID = ?";
-    $stmt = $conn->prepare($query);
+    $materialsQuery = "SELECT MaterialID, MaterialName FROM RawMaterials WHERE SupplierID = ?";
+    $stmt = $conn->prepare($materialsQuery);
     $stmt->bind_param("i", $supplierID);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    echo "<option value=''>Select Material</option>";
     while ($row = $result->fetch_assoc()) {
         echo "<option value='" . $row['MaterialID'] . "'>" . $row['MaterialName'] . "</option>";
     }
+    $stmt->close();
 }
 ?>
